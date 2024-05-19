81dfc493b23d0e3cef019d8536dd098dbc97154e
sandzyaru <sanchous048@gmail.com>
2024-05-19 21:16:52 +0600

    nothing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import HomePage from './pages/HomePage/HomePage'
 import Navbar from './components/Navbar/Navbar'
 import UnderNavbar from './components/UnderNavbar/UnderNavbar'
 import Routing from './Routing'
 import Card from './components/Card/Card'
+import { json } from 'react-router-dom'
 
 const App = () => {
+
+  const [data, setData] = useState(null) 
+
+  // useEffect(() => {
+  //   fetch('/api')
+  //   .then((response) => response.json())
+  //   .then(response => setData())
+  // }, [])
+
   return (
     <>
       <Navbar />
